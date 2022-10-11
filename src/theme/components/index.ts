67c7c858e300cc8f5ceecb8b534de780e60c3af0
slava <slavik-28@mail.ru>
2022-10-11 16:05:36 +0300

    refactor

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,12 +1,8 @@
 import { Components } from '@mui/material/styles'
 import MuiCssBaseline from 'theme/components/global'
 import MuiButton from 'theme/components/button'
-import MuiCheckbox from 'theme/components/checkbox'
-// import MuiInputLabel from 'theme/components/inputLabel'
 
 export const components = {
   MuiCssBaseline,
   ...MuiButton,
-  ...MuiCheckbox,
-  // ...MuiInputLabel,
 } as Partial<Components>
